b9747435c2f376134a4926f14879566a4507e95f
JehielL <jehiel.py@gmail.com>
2024-01-08 17:49:38 +0100

    Initial commit

---
+++ src/com/praticasjava/BuclesFor.java
@@ -0,0 +1,19 @@
+package com.praticasjava;
+
+public class BuclesFor {
+
+    public static void main(String[] args) {
+
+        String[] phones = new String[4];
+        phones[0] = "643907051";
+        phones[1] = "643907052";
+        phones[2] = "643907053";
+        phones[3] = "643907054";
+
+        for (int i = 0; i < phones.length; i++){
+            System.out.println(phones[i]);
+        }
+
+
+    }
+}
